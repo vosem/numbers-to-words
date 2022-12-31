72af980fe5ccfdccc3ca02b747eb3df19621c355
Anna <anna.v.popovska@gmail.com>
2022-12-31 15:37:58 +0100

    Add styles to keyboard and input

--- client/src/components/Keyboard.tsx
+++ client/src/components/Keyboard.tsx
@@ -1,42 +1,83 @@
 import React, { useState } from 'react';
+import { ReactComponent as Arrow } from './images/arrow-left.svg';
+
+import config from './config.json';
 
 import styles from './styles.module.scss';
 
 type Props = {
     setWords: (numbers: Array<string>) => void;
-}
+};
 
-const URL = "http://localhost:9000/convert";
+const URL = 'http://localhost:9000/convert';
 
 const Keyboard = ({ setWords }: Props): JSX.Element => {
     const [numbers, setNumbers] = useState<string>('');
-    const numbersArray: Array<string> = Array
-        .from({length: 10}, (_, index) => index < 9 ? (index + 1).toString() : '0');
+    const { buttons } = config;
 
-  const convert = async (number: string): Promise<void> => {
-    // e.preventDefault();
-    const newNumbersSet = numbers.concat(number);
-    const response = await fetch(URL + '?' + new URLSearchParams({numbers: newNumbersSet}).toString());
-    const { data } = await response.json();
+    const sendRequest = async (numbers: string): Promise<void> => {
+        const response = await fetch(URL + '?' + new URLSearchParams({ numbers: numbers }).toString());
+        const { data } = await response.json();
 
-    setNumbers(newNumbersSet);
-    setWords(data);
-  }
+        setWords(data);
+    }
 
-  return (
-      <div className={styles.Wrapper}>
-        <div className={styles.Keyboard}>
-            {numbersArray.map(number => (
-                <div
-                    key={number}
-                    onClick={() => convert(number)}
-                >
-                    {number}
-                </div>
-            ))}
+    const convert = async (number: string): Promise<void> => {
+        const newNumbersSet = numbers.concat(number);
+
+        setNumbers(newNumbersSet);
+
+        if (newNumbersSet.length) {
+            await sendRequest(newNumbersSet);
+        }
+    };
+
+    const removeNumber = async (): Promise<void> => {
+        const newNumbersSet = numbers.slice(0, -1);
+        console.log('newNumbersSet', newNumbersSet)
+
+        setNumbers(newNumbersSet);
+
+        if (newNumbersSet.length) {
+            await sendRequest(newNumbersSet);
+        }
+    };
+
+    return (
+        <div className={styles.Wrapper}>
+            <input
+                type="text"
+                className={styles.Input}
+                name="ask-question"
+                maxLength={15}
+                value={numbers}
+                disabled={true}
+            />
+            <div className={styles.Keyboard}>
+                {buttons.map((button, index) => {
+                    const number = Object.keys(button)[0];
+                    const letters = Object.values(button)[0];
+
+                    return (
+                        <button
+                            key={number}
+                            onClick={index === buttons.length - 1 ? removeNumber : () => convert(number)}
+                            className={styles.Button}
+                            disabled={number === '*'}
+                        >
+                            <p className={styles.Number}>
+                                {number}
+                                {index === buttons.length - 1 && <Arrow />}
+                            </p>
+                            <p className={styles.Letters}>
+                                {letters}
+                            </p>
+                        </button>
+                    )
+                })}
+            </div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default Keyboard;
